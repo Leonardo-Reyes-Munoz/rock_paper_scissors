1a6f6961734eed79b1d63073b37e295e402fa59c
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-01-21 18:32:04 -0800

    Adds function to select player choice and compare it to computer choice. Alerts won or lost message

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,13 +1,40 @@
+//returns random choice from list of options for computer
 function getComputerChoice () {
-    let selection = Math.floor((Math.random() * 3)); //selects random number whole number between zero and two (not including three)
-    computerChoice = options[selection];
+    let computerSelection = Math.floor((Math.random() * 3)); //selects random number whole number between zero and two (not including three)
+    computerChoice = options[computerSelection];
     return computerChoice;
 }
 
+//
+function playRound(playerChoice, computerChoice) {
+    playerSelection = prompt("Choose between rock, paper, or scissors:");
+    playerChoice = playerSelection.toLowerCase();
+    computerChoice = getComputerChoice();
+
+    if (playerChoice === computerChoice) {
+        alert("It's a draw!");
+    } else if (playerChoice === "rock"  && computerChoice === "scissors") {
+        alert("You win! Rock beats scissors");
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        alert("You win! Paper beats Rock.");
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        alert("You win! Scissors beats paper.")
+    } else if (computerChoice === "rock"  && playerChoice === "scissors") {
+        alert("You lose! Scissors loses to rock.");
+    } else if (computerChoice === "paper" && playerChoice === "rock") {
+        alert("You lose! Rock loses to paper.");
+    } else if (computerChoice === "scissors" && playerChoice === "paper") {
+        alert("You lose! Paper loses to scissors.")
+    } else {
+        alert("You entered an invalid option. You forfeit");
+    }
+    console.log(playerChoice);
+    console.log(computerChoice);
+}
+
 const options = ["rock", "paper", "scissors"];
+let playerChoice = " ";
 let computerChoice = " ";
 
-getComputerChoice();
-console.log(computerChoice);
-
+playRound(playerChoice, computerChoice);
 
