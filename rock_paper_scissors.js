64355060af017b4e175bac0eb1be0b576593f52b
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-02-28 19:19:11 -0800

    Adds javaScript to change game logic from user input to event listeners.

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -5,37 +5,76 @@ function getComputerChoice () {
     return computerChoice;
 }
 
-//Prompts for player choice and compares it to computer choice. Alets won or lost message. 
+//Prompts for player choice and compares it to computer choice. Alerts won or lost message. 
 // Adds to computer or player score. Returns score.
 function playRound(playerChoice, computerChoice) {
-    playerSelection = prompt("Choose between rock, paper, or scissors:");
-    playerChoice = playerSelection.toLowerCase();
+    //playerSelection 
+    console.log(this.id);
+    if(this.id === "rockbtn") {
+        playerChoice = "rock";
+    } else if(this.id === "paperbtn") {
+        playerChoice = "paper";  
+    } else if(this.id === "scissorsbtn") {
+        playerChoice = "scissors";
+    };
+
     computerChoice = getComputerChoice();
 
     if (playerChoice === computerChoice) {
         alert("It's a draw!");
     } else if (playerChoice === "rock"  && computerChoice === "scissors") {
         alert("You win! Rock beats scissors");
-        return playerScore++;
+        playerScore++;
     } else if (playerChoice === "paper" && computerChoice === "rock") {
         alert("You win! Paper beats Rock.");
-        return playerScore++;
+        playerScore++;
     } else if (playerChoice === "scissors" && computerChoice === "paper") {
         alert("You win! Scissors beats paper.")
-        return playerScore++;
+        playerScore++;
     } else if (computerChoice === "rock"  && playerChoice === "scissors") {
         alert("You lose! Scissors loses to rock.");
-        return computerScore++;
+        computerScore++;
     } else if (computerChoice === "paper" && playerChoice === "rock") {
         alert("You lose! Rock loses to paper.");
-        return computerScore++;
+        computerScore++;
     } else if (computerChoice === "scissors" && playerChoice === "paper") {
         alert("You lose! Paper loses to scissors.")
-        return computerScore++;
+        computerScore++;
     } else {
         alert("You entered an invalid option. You lose!");
-        return computerScore++;
+        computerScore++;
     }
+
+    const container = document.querySelector("#container");
+
+    const content = document.createElement('div');
+    content.classList.add("content");
+    content.textContent = `Computer Score ${computerScore} Player Score ${playerScore}`;
+
+    container.appendChild(content);
+    
+    console.log(computerScore);
+    console.log(playerScore);
+    
+    
+    //Compares playerScore to computerScore once the combine score reaches 5.
+    //clears scores once the total score reaches 5 and winner is determined 
+    if(playerScore + computerScore === 5) {
+        if (playerScore > computerScore) {
+            alert("Congratulations you won best out of 5. The game will restart now.");
+            content.textContent = `Final Computer Score ${computerScore} Final Player Score ${playerScore}`;
+            computerScore = 0;
+            playerScore = 0;
+        } else {
+            alert("You lost. Computer won best out of 5. The game will restart now.");
+            content.textContent = `Final Computer Score ${computerScore} Final Player Score ${playerScore}`;
+            computerScore = 0;
+            playerScore = 0;
+        }
+    }
+    //score loops by clearing the score
+
+
 }
 
 const options = ["rock", "paper", "scissors"];
@@ -44,20 +83,16 @@ let computerChoice = " ";
 let computerScore = 0;
 let playerScore = 0;
 
-//initiates game and continues for 5 rounds.
-function game() {
-    for (let i = 0; i < 5 ; i++) {
-        playRound(playerChoice, computerChoice);
-    }
-}
- game();
 
- //compares scores and if player won or lost. 
- if (playerScore > computerScore) {
-    alert("Congratulations you won best out of 5.");
- } else {
-    alert("You lost. Computer won best out of 5.");
- }
+const rockbtn = document.querySelector('#rockbtn');
+rockbtn.addEventListener('click', playRound);
 
-//end of game
+const paperbtn = document.querySelector('#paperbtn');
+paperbtn.addEventListener('click', playRound);
+
+const scissorsbtn = document.querySelector('#scissorsbtn');
+scissorsbtn.addEventListener('click', playRound);
+
+
+
 
